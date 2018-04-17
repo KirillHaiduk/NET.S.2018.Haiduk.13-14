335503a0488cbd5c2a88299d6f7a4129510d8008
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-17 18:28:28 +0300

    Update CustomQueue.cs

--- Day13-14Tasks1-4/CustomQueue.cs
+++ Day13-14Tasks1-4/CustomQueue.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System;
 
 namespace Day13_14Tasks1_4
 {
-    public class CustomQueue<T> : IEnumerable<T>, ICollection, IEnumerable
-    {
+    public class CustomQueue<T>
+    { 
         private T[] array;
         private int head;
         private int tail;
         private int size;
+        private int version = 0;
 
+        /// <summary>
+        /// Constructor without parameters
+        /// </summary>
         public CustomQueue()
         {
         }
 
+        /// <summary>
+        /// Constructor for queue instance that accepts capacity
+        /// </summary>
+        /// <param name="capacity">Capacity of sz-array for queue</param>
         public CustomQueue(int capacity)
         {
             if (capacity < 0)
@@ -28,43 +34,113 @@ namespace Day13_14Tasks1_4
             size = 0;
         }
 
+        /// <summary>
+        /// Property for getting number of queue elements
+        /// </summary>
         public int Count => size;
 
-        public object SyncRoot => throw new NotImplementedException();
+        /// <summary>
+        /// Property for getting version of queue
+        /// </summary>
+        public int Version => version;
 
-        public bool IsSynchronized => false;
-
-        public void CopyTo(Array array, int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Method for adding element to the end of queue
+        /// </summary>
+        /// <param name="item">Element to add</param>
         public void Enqueue(T item)
         {
             array[tail] = item;
             tail = (tail + 1) % array.Length;
             size++;
+            version++;
         }
 
+        /// <summary>
+        /// Method for removing and returning the object at the beginning of the queue
+        /// </summary>
+        /// <returns>The object that is removed from the beginning of the queue</returns>
         public T Dequeue()
         {
             T result = array[head];
             array[head] = default(T);
             head = (head + 1) % array.Length;
             size--;
+            version++;
             return result;
         }
 
-        public T Peek() => array[head];
+        /// <summary>
+        /// Method for returning the object at the beginning of the queue
+        /// </summary>
+        /// <returns>The object at the beginning of the queue</returns>
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return array[head];
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the queue
+        /// </summary>
+        /// <returns>Instance of structure for iterating</returns>
+        public CustomIterator GetEnumerator()
+        {
+            return new CustomIterator(this);
+        }
+
+        /// <summary>
+        /// Struct that contains methods for supporting <see langword="foreach"/> operator
+        /// </summary>
+        public struct CustomIterator
+        {
+            private readonly CustomQueue<T> queue;
+            private int currentIndex;
+
+            public CustomIterator(CustomQueue<T> collection)
+            {
+                currentIndex = -1;
+                queue = collection;
+            }
+
+            /// <summary>
+            /// Gets the element in the queue at the current position of the enumerator
+            /// </summary>
+            public T Current
+            {
+                get
+                {
+                    if (currentIndex == -1 || currentIndex == queue.Count)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    return queue.array[currentIndex];
+                }
+            }
+
+            /// <summary>
+            /// Sets the enumerator to its initial position, which is before the first element in the collection
+            /// </summary>
+            public void Reset() => currentIndex = -1;
+
+            /// <summary>
+            /// Advances the enumerator to the next element of the queue
+            /// </summary>
+            /// <returns>True if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the queue</returns>
+            public bool MoveNext()
+            {
+                //if (queue.version != version)
+                //{
+                //    throw new InvalidOperationException();
+                //}
+
+                return ++currentIndex < queue.Count;
+            }
+        }
     }
 }
