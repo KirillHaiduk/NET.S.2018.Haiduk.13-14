8b51d1b6a89ce63ad0a31d8ec81a75db5f3d30b8
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-17 18:30:56 +0300

    Update FibonacciTests.cs

--- Day13-14Tasks1-4.Tests/FibonacciTests.cs
+++ Day13-14Tasks1-4.Tests/FibonacciTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Numerics;
 using NUnit.Framework;
 using static Day13_14Tasks1_4.Fibonacci;
 
@@ -9,13 +10,20 @@ namespace Day13_14Tasks1_4.Tests
     {
         [TestCase(-100)]
         [TestCase(0)]
-        public void FibonacciGeneratorTest_AcceptsZeroOrNegativeLimit_ThrowsException(long limit) => Assert.Throws<ArgumentException>(() => FibonacciGenerator(limit));
-
-        [TestCase(377, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 })]
-        [TestCase(11000, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946 })]        
-        public void FibonacciGeneratorTest(long limit, long[] array)
+        public void FibonacciGeneratorTest_AcceptsZeroOrNegativeLimit_ThrowsException(int amount) => Assert.Throws<ArgumentException>(() => FibonacciGenerator(amount));
+                    
+        [Test]
+        public void FibonacciGeneratorTest1()
         {
-            CollectionAssert.AreEqual(array, FibonacciGenerator(limit).ToArray());
+            var b = new BigInteger[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 };
+            CollectionAssert.AreEqual(b, FibonacciGenerator(15));
+        }
+
+        [Test]
+        public void FibonacciGeneratorTest2()
+        {
+            var b = new BigInteger[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946 };
+            CollectionAssert.AreEqual(b, FibonacciGenerator(22));
         }
     }
 }
